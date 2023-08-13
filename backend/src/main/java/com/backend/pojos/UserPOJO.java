b7e346e6eeac524f30223d96d653373881b1d9d5
Divanshu34 <divanshujain3451@gmail.com>
2023-08-13 19:05:18 +0530

    UserPOJO modified and added fields, getters, setters, toString...

--- backend/src/main/java/com/backend/pojos/UserPOJO.java
+++ backend/src/main/java/com/backend/pojos/UserPOJO.java
@@ -1,13 +1,96 @@
 package com.backend.pojos;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class UserPOJO {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
-    private UserRole roles;
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
+    @Column(name = "is_present")
+    private Boolean isPresent;
+
+    public UserPOJO() {
+    }
+
+    public UserPOJO(String firstName, String lastName, UserRole role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRoles(UserRole role) {
+        this.role = role;
+    }
+
+    public Boolean getIsPresent() {
+        return isPresent;
+    }
+
+    public void setIsPresent(Boolean isPresent) {
+        this.isPresent = isPresent;
+    }
+
+    @Override
+    public String toString() {
+        String message = "Invalid User";
+        if (this.getIsPresent()) {
+            message = toStr();
+        }
+        return message;
+    }
+
+    private String toStr() {
+        return "UserPOJO [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", role=" + role
+                + "]";
+    }
 }
