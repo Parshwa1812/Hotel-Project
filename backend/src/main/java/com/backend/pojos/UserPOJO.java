60f97563d71568ab597097b1def0ecd05dcc193a
Divanshu34 <divanshujain3451@gmail.com>
2023-08-13 18:39:32 +0530

    created UserRole enum type in pojos package

--- backend/src/main/java/com/backend/pojos/UserPOJO.java
+++ backend/src/main/java/com/backend/pojos/UserPOJO.java
@@ -2,6 +2,12 @@ package com.backend.pojos;
 
 public class UserPOJO {
     
-    
+    private Long userId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private UserRole roles;
 
 }
